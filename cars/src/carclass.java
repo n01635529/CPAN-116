76df92deff520a3d3a4396dc31f00d213cd1b053
n01635529 <n01635529@humber.ca>
2023-10-12 21:15:14 -0400

    for lab3

--- cars/src/carclass.java
+++ cars/src/carclass.java
@@ -1,6 +1,36 @@
-package algorithms.cars.src;
+package cars.src;
 
 public class carclass {
+    String make;
+    String model;
+    String year;
+    String color;
+    String status = "stopped";
+    int speed = 0;
+    
+    public void start(){
+        System.out.println("Starting the car");
+        this.status = "started";
+        
+    }
+    
+    public void stop(){
+        System.out.println("Stopping the car");
+        this.status = "stopped";
+        
+    }
+    
+    public void honk(){
+        System.out.println("honk honk~~~");
+       
+    }
 
+    public void accelerate(){
+        this.speed = + 20;
+    }
+    
+    public void brake(){
+        this.speed = - 10;
+    }
 }
-
+ 
--- cars/src/cars.java
+++ cars/src/cars.java
@@ -1,5 +1,35 @@
-package algorithms.cars.src;
+package cars.src;
 
 public class cars {
+    public static void main(String[] args) {
+
+        carclass my_car = new carclass();
+        my_car.make = "Subaru";
+        my_car.model = "Legacy";
+        my_car.year = "2012";
+        my_car.color = "Black";
+        
+        System.out.println(my_car.make);
+        System.out.println(my_car.model);
+        System.out.println(my_car.year);
+        System.out.println(my_car.color);
+
+        System.out.println(my_car.status);
+        System.out.println(my_car.speed);
+
+        my_car.start();
+        System.out.println(my_car.status);
+        System.out.println(my_car.speed);
+
+        my_car.accelerate();
+        System.out.println(my_car.speed);
+
+        my_car.brake();
+        my_car.brake();
+        System.out.println(my_car.status);
+        System.out.println(my_car.speed);
+        my_car.honk();
+    }
     
 }
+ 
